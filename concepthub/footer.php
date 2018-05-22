4ba2ebe7998c39c221c15881aedd6c12fe36a1cf
Niklas Clemens Noldin <nnoldin.mmt-b2017@fh-salzburg.ac.at>
2018-05-22 21:58:50 +0200

    footer.php

--- concepthub/footer.php
+++ concepthub/footer.php
@@ -1,6 +1,7 @@
 <footer>
 
-    <p>Der Concepthub ist im Rahmen des Multimedia-Projektes 1 des Studiengangs Multimedia Technology von Niklas Noldin konzipiert und umgesetzt worden.</p>
+    <p>Der Concepthub ist im Rahmen des Multimedia-Projektes 1 des <a href="https://multimediatechnology.at/"> Studiengangs Multimedia Technology</a> an der <a href="https://www.fh-salzburg.ac.at/">Fachhochschule Salzburg</a> von Niklas Noldin konzipiert und umgesetzt worden.</p>
+    <p>Bist du ein libanesischer Prinz, auf der Suche nach einer Möglichkeit dein Geld zu waschen? <a href="paypal.me/niklasnoldin">Bitte hier drücken</a></p>
     
 
 
