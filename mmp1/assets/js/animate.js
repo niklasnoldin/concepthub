99b4e4320598d7cf76dc99f627a65a3ab5e9e630
Niklas Clemens Noldin <nnoldin.mmt-b2017@fh-salzburg.ac.at>
2018-06-13 01:40:51 +0200

    finetuning

--- mmp1/assets/js/animate.js
+++ mmp1/assets/js/animate.js
@@ -1,21 +1,30 @@
 function animate(){
     const mainLogo = document.querySelector('header');
-    animation();
+    animationIn();
+    animationOut();
 
     mainLogo.addEventListener('mouseenter', function(){
-        animation();
+        animationIn();
+    })
+    mainLogo.addEventListener('mouseleave', function(){
+        animationOut();
     })
 }
-function animation(){
-    TweenMax.to("[data-name='1_left']", .75, { x: 26, y: -7.5, yoyo: true, repeat: 1, easing: Power3.easeInOut });
-    TweenMax.to("[data-name='1_left']", .5, { x: 0, y: 0, delay: 1, easing: Elastic.easeInOut});
+function animationIn(){
+    TweenMax.to("[data-name='1_left']", .75, { x: 26, y: -7.5, ease: Bounce.easeOut });
 
-    TweenMax.to("[data-name='3_left']", .75, { x: 26, y: -7.5, yoyo: true, repeat: 1, easing: Power3.easeInOut });
-    TweenMax.to("[data-name='3_left']", .5, { x: 0, y: 0, delay: 1, easing: Elastic.easeInOut});
+    TweenMax.to("[data-name='3_left']", .75, { x: 26, y: -7.5, ease: Bounce.easeOut });
     
-    TweenMax.to("[data-name='2_right']", .75, { x: -26, y: 7.5, yoyo: true, repeat: 1, easing: Power3.easeInOut });
-    TweenMax.to("[data-name='2_right']", .5, { x: 0, y: 0, delay: 1, easing: Elastic.easeInOut});
+    TweenMax.to("[data-name='2_right']", .75, { x: -26, y: 7.5, ease: Bounce.easeOut });
     
-    TweenMax.to("[data-name='4_right']", .75, { x: -26, y: 7.5, yoyo: true, repeat: 1, easing: Power3.easeInOut });
-    TweenMax.to("[data-name='4_right']", .5, { x: 0, y: 0, delay: 1, easing: Elastic.easeInOut});
+    TweenMax.to("[data-name='4_right']", .75, { x: -26, y: 7.5, ease: Bounce.easeOut });
+}
+function animationOut(){
+    TweenMax.to("[data-name='1_left']", .75, { x: 0, y: 0, ease: Elastic.easeOut.config( 1, 0.3)});
+
+    TweenMax.to("[data-name='3_left']", .75, { x: 0, y: 0, ease: Elastic.easeOut.config( 1, 0.3)});
+    
+    TweenMax.to("[data-name='2_right']", .75, { x: 0, y: 0, ease: Elastic.easeOut.config( 1, 0.3)});
+    
+    TweenMax.to("[data-name='4_right']", .75, { x: 0, y: 0, ease: Elastic.easeOut.config( 1, 0.3)});
 }
